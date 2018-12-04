bf2a3821827424b507216cc159e0e9f1bb48befb
jiangke <jiangkeredgirl@hotmail.com>
2018-12-05 00:12:35 +0800

    增加qt build 脚本

---
+++ qtbuild.bat
@@ -0,0 +1,4 @@
+C:\Qt\Qt5.11.0\5.11.0\msvc2017_64\bin\qmake.exe D:\develop\myproject\dailybuild\untitled\untitled.pro -spec win32-msvc "CONFIG+=debug" "CONFIG+=qml_debug"
+C:\Qt\Qt5.11.0\Tools\QtCreator\bin\jom.exe
+C:\Qt\Qt5.11.0\Tools\QtCreator\bin\jom.exe clean
+pause
